65c2194072b59d2b3cc34a8127b23aecf520b3f6
Arithra Basu <aritra.basu@godeskless.com>
2023-02-04 13:54:50 +0530

    auth changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React,{useEffect,useState} from 'react';
-import {useStateValue} from './StateProvider'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useStateValue } from "./StateProvider";
+import "./App.css";
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,120 +8,121 @@ import {
   BrowserRouter,
   Redirect,
 } from "react-router-dom";
-import Profile from './routes/Profile';
-import Login from './routes/Login';
-import firebaseConfig,{fire,db} from './routes/firebase'
-import  {PrivateRoute} from './routes/PrivateRoute';
-import Home from './routes/Home';
-import Timeline from './routes/TImeline';
-import Classrooms from './routes/Classrooms';
-import Classroom from './routes/Classroom';
-import ProfileList from './routes/ProfileList';
+import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import firebaseConfig, { fire, db } from "./routes/firebase";
+import { PrivateRoute } from "./routes/PrivateRoute";
+import Home from "./routes/Home";
+import Timeline from "./routes/TImeline";
+import Classrooms from "./routes/Classrooms";
+import Classroom from "./routes/Classroom";
+import ProfileList from "./routes/ProfileList";
 function App() {
-  var gapi=window.gapi;
-  const[,dispatch]=useStateValue();
+  var gapi = window.gapi;
+  const [, dispatch] = useStateValue();
   const [userData, setUser] = useState(null);
   useEffect(() => {
     fire.auth().onAuthStateChanged((user) => {
       // console.log(user)
-       if(user)
-       { 
-         gapi.load('client',()=>{
-              gapi.client.init({
-                apiKey: firebaseConfig.apiKey,
-                clientId: firebaseConfig.clientId,
-                discoveryDocs: firebaseConfig.discoveryDocs,
-                scope: firebaseConfig.scope,
-              }).then(function() {
-                // Make sure the Google API Client is properly signed in
-                if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
-                  startApp(user);
-                } else {
-                  fire.auth().signOut(); // Something went wrong, sign out
-                }
-              })
-         })
-                                   
-      
-         localStorage.setItem('uid',user.uid);
-         localStorage.setItem('userName',user.displayName);
-         UserData(user);
-         setUser({user});
-       }else
-       {
-         localStorage.removeItem('uid');
-         localStorage.removeItem('userName');
-         setUser({});
-       }
-     });
-     function UserData(userdata)
-    {
-      var user= null  
-            const ProfileRef = db.collection('profile').doc(userdata.uid)
-             ProfileRef.get()
-                .then((doc) => {
-                    if (doc.exists) {
-                        //doc already exists
-                        user=doc.data();
-                        dispatch({
-                          type:'USER_SIGN_UP',
-                          item:user
-                          });
-                    } else {
-                        //create profile
-                        ProfileRef.set({
-                            uId:userdata.uid,
-                            Dname:userdata.displayName,
-                            email:userdata.email,
-                            photoURL:userdata.photoURL,
-                            role:'student',
+      if (user) {
+        gapi.load("client", () => {
+          gapi.client
+            .init({
+              apiKey: firebaseConfig.apiKey,
+              clientId: firebaseConfig.clientId,
+              discoveryDocs: firebaseConfig.discoveryDocs,
+              scope: firebaseConfig.scope,
+            })
+            .then(function () {
+              // Make sure the Google API Client is properly signed in
+              if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                startApp(user);
+              } else {
+                fire.auth().signOut(); // Something went wrong, sign out
+              }
+            });
+        });
 
-                        })
-                        .then(function(docRef) {
-                          user={
-                            uId:userdata.uid,
-                            Dname:userdata.displayName,
-                            email:userdata.email,
-                            photoURL:userdata.photoURL,
-                            role:userdata.role
-                        }
-                        dispatch({
-                          type:'USER_SIGN_UP',
-                          item:user
-                          });
-                        })
-                        .catch(function(error) {
-                            console.error("Error adding document: ", error);
-                        });
-                    }
-                }).catch(function(error) {
-                    console.log("Error getting document:", error);
+        localStorage.setItem("uid", user.uid);
+        localStorage.setItem("userName", user.displayName);
+        UserData(user);
+        setUser({ user });
+      }
+      //  else
+      //  {
+      //    localStorage.removeItem('uid');
+      //    localStorage.removeItem('userName');
+      //    setUser({});
+      //  }
+    });
+    function UserData(userdata) {
+      var user = null;
+      const ProfileRef = db.collection("profile").doc(userdata.uid);
+      ProfileRef.get()
+        .then((doc) => {
+          if (doc.exists) {
+            //doc already exists
+            user = doc.data();
+            dispatch({
+              type: "USER_SIGN_UP",
+              item: user,
+            });
+          } else {
+            //create profile
+            ProfileRef.set({
+              uId: userdata.uid,
+              Dname: userdata.displayName,
+              email: userdata.email,
+              photoURL: userdata.photoURL,
+              role: "student",
+            })
+              .then(function (docRef) {
+                user = {
+                  uId: userdata.uid,
+                  Dname: userdata.displayName,
+                  email: userdata.email,
+                  photoURL: userdata.photoURL,
+                  role: userdata.role,
+                };
+                dispatch({
+                  type: "USER_SIGN_UP",
+                  item: user,
                 });
-                
-           }
-    }, [])
-    function startApp(user) {
-      fire.auth().currentUser.getIdToken()
-      .then(function(token) {
-        dispatch({
-          type:'ADD_GOOGLE_AUTH_TOKEN',
-          item:token
+              })
+              .catch(function (error) {
+                console.error("Error adding document: ", error);
+              });
+          }
         })
-       })
-      .then(function(response) {
-        console.log(response);  
-      });
+        .catch(function (error) {
+          console.log("Error getting document:", error);
+        });
     }
+  }, []);
+  function startApp(user) {
+    fire
+      .auth()
+      .currentUser.getIdToken()
+      .then(function (token) {
+        dispatch({
+          type: "ADD_GOOGLE_AUTH_TOKEN",
+          item: token,
+        });
+      })
+      .then(function (response) {
+        console.log(response);
+      });
+  }
   return (
     <div className="App">
-       <Router>
+      <Router>
         <Switch>
           <PrivateRoute path="/profile/:profileid" component={Profile} />
           <PrivateRoute path="/profiles" component={ProfileList} />
           <PrivateRoute path="/timeline" component={Timeline} />
           <PrivateRoute path="/classrooms" component={Classrooms} />
           <PrivateRoute path="/classroom/:classRoomId" component={Classroom} />
-          
+
           <Route path="/login">
             {/* <Login /> */}
             <Login />
@@ -131,7 +132,6 @@ function App() {
           </Route>
         </Switch>
       </Router>
-     
     </div>
   );
 }
--- src/components/Drive.js
+++ src/components/Drive.js
@@ -1,64 +1,75 @@
-import React from 'react'
-import {db} from '../routes/firebase'
-var gapi= window.gapi;
+import React from "react";
+import { db } from "../routes/firebase";
+var gapi = window.gapi;
 
-async function Drive (action)
-{
-    gapi.load('client', async function () {
-        gapi.client.load('drive', 'v3',async function (){
-            switch(action.type)
-           {
-                case 'UPLOAD_FILE' :
-                    var uploadfileres = await uploadFile(action);
-                    return uploadfileres
-                default :
-                 return true ;
-           }
-        })
-    })
-
+async function Drive(action) {
+  gapi.load("client", async function () {
+    gapi.client.load("drive", "v3", async function () {
+      switch (action.type) {
+        case "UPLOAD_FILE":
+          var uploadfileres = await uploadFile(action);
+          return uploadfileres;
+        default:
+          return true;
+      }
+    });
+  });
 }
 
-async function uploadFile(item)
-{
-    var form = new FormData();
-    form.append('metadata', new Blob([JSON.stringify(item.metadata)], {type: 'application/json'}));
-    form.append('file', item.file);
-    var result = fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
-                        method: 'POST',
-                        headers: new Headers({'Authorization': 'Bearer ' + gapi.auth.getToken().access_token}),
-                        body: form
-                        }).then((res) => {
-                            
-                            console.log(res);
-                            return res.json();
-                            }).then(function(val) {
+async function uploadFile(item) {
+  console.log("uploading fileeee");
+  var form = new FormData();
+  form.append(
+    "metadata",
+    new Blob([JSON.stringify(item.metadata)], { type: "application/json" })
+  );
+  form.append("file", item.file);
+  var result = fetch(
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+    {
+      method: "POST",
+      headers: new Headers({
+        Authorization: "Bearer " + gapi.auth.getToken().access_token,
+      }),
+      body: form,
+    }
+  )
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then(function (val) {
+      console.log("val", val);
+      gapi.client.drive.files
+        .get({
+          fileId: val.id,
+          fields: "webContentLink",
+        })
+        .then(
+          function (success) {
+            console.log(success.result.webContentLink);
+            var embedLink = success.result.embedLink;
+            if (item.firebase) {
+              switch (item.firebase.type) {
+                case "UPDATE":
+                  var dbRef = db
+                    .collection(item.firebase.collection)
+                    .doc(item.firebase.doc);
+                  dbRef.update({
+                    [item.firebase.feild]: embedLink,
+                  });
 
-                                gapi.client.drive.files.get({
-                                    fileId: val.id,
-                                    fields: 'embedLink'
-                                }).then(function(success){
-                                    console.log(success)
-                                    var embedLink = success.result.embedLink;    
-                                    if(item.firebase)
-                                    {
-                                        switch(item.firebase.type)
-                                        {
-                                            case 'UPDATE':
-                                                    var dbRef = db.collection(item.firebase.collection).doc(item.firebase.doc);
-                                                    dbRef.update({
-                                                        [item.firebase.feild]: embedLink
-                                                    })
-
-                                            default:
-                                                return false
-                                            
-                                        }
-                                    }
-                                }, function(fail){
-                                    console.log('Error '+ fail.result.error.message);
-                                })
-                            });
+                default:
+                  return false;
+              }
+            }
+          },
+          function (fail) {
+            console.log("Error " + fail.result.error.message);
+          }
+        )
+        .catch((e) => console.log(e));
+    });
 }
 
 export default Drive;
